9d6dca09be43fd9f9e00165cf88e549ca941ae46
oneofminyards <71725779+oneofminyards@users.noreply.github.com>
2020-10-05 12:29:43 +0300

    Initial commit

---
+++ TheLandOfDragons/Assets/Scripts/Step.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+    [TextArea]
+    public string shablon;
+
+    public Step win;
+    public Step lose;
+}
---
+++ TheLandOfDragons/Assets/Scripts/TheLandOfDragons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheLandOfDragons : MonoBehaviour
+{
+    public Text contentText;
+    public Step startStep;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            contentText.text = "WELCOME TO THE LAND OF DRAGONS. FOLLOW THE TREASURE MAP, MAKE WISE CHOICES AND FIND DRAGONS' DARKEST SECRETS"
+                + "(press ENTER to continue the game)";
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+          contentText.text = startStep.shablon;
+        }
+
+       if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            contentText.text = startStep.win.shablon;
+        }
+       if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            contentText.text = startStep.lose.shablon;
+        }
+    }
+}
